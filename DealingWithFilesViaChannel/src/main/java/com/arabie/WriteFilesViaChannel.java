a7794aa5cfeffe6e10d908ef7389af1d3d4675a3
ahmedarabie13 <engarabie13@gmail.com>
2021-02-05 17:57:12 +0200

    Day3 lab2

---
+++ DealingWithFilesViaChannel/src/main/java/com/arabie/WriteFilesViaChannel.java
@@ -0,0 +1,27 @@
+package com.arabie;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.file.Files;
+import java.nio.file.InvalidPathException;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+
+public class WriteFilesViaChannel {
+    public static void main(String args[]) {
+        try (FileChannel fChan = (FileChannel) Files.newByteChannel(Paths.get("output.txt"), StandardOpenOption.WRITE,
+                StandardOpenOption.CREATE)) {
+            ByteBuffer mBuf = ByteBuffer.allocate(26);
+            for (int i = 0; i < 26; i++)
+                mBuf.put((byte) ('A' + i));
+            mBuf.rewind();
+            fChan.write(mBuf);
+        } catch (InvalidPathException e) {
+            System.out.println("Path Error " + e);
+        } catch (IOException e) {
+            System.out.println("I/O Error: " + e);
+            System.exit(1);
+        }
+    }
+}
